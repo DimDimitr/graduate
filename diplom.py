b6c26e6429be992eb92f0294ff48a56bb9d70f80
Ефремов Дмитрий <Vkodima.le@gmail.com>
2019-04-11 15:58:54 +0300

    Добавлен GUI Добавлен поиск оптимального значения

--- diplom.py
+++ diplom.py
@@ -14,8 +14,8 @@ from tkinter import tix as tk
 from tkinter import *
 from tkinter import ttk
 
-
-incomeMidTime = 5
+repeatCount = 20
+incomeMidTime = 1
 
 #Класс описывающий среднее свремя по категориям для отдельного типа обращения
 class BaseTime():
@@ -136,7 +136,6 @@ class InputQueueEngine():
     
 #    Вернет операцию, если человек пришел, None если нет
     def inputQueueGet(self):
-#        print("в очереди на приход - " + str(len(self.commingQueue)))
         if(len(self.commingQueue) > 0):
             return self.commingQueue[0].increaseTime()
         
@@ -166,7 +165,6 @@ class Till():
     def __increaseServiceCount(self):
         self.serviceCount += 1
     def changeStatus(self):
-#        print("Till isVacant was and become " + str(self.isVacant) + str(not self.isVacant))
         self.isVacant = not self.isVacant
         return self.isVacant
     
@@ -241,7 +239,6 @@ class GeneralStandartQueue():
             if (self.queueMaxLen < actualLength):
                     self.queueMaxLen = actualLength
             self.sortInQueuePart(self.generalQueue[len(self.generalQueue) - 1])
-#        print("after add" + str(self.generalQueue))
     
     def getLengthCode(self):
         if ((self.generalQueue) == None):
@@ -269,14 +266,10 @@ class GeneralStandartQueue():
         tempTr = []
         for party in queuePart:
             tempTr.append(party.queueUnit.operation.getOperation_time())
-#        print('before')
-#        print(tempTr)
         queuePart = sorted(queuePart, key=lambda queueUnit: queueUnit.queueUnit.operation.getOperation_time())
         tempTr = []
         for party in queuePart:
             tempTr.append(party.queueUnit.operation.getOperation_time())
-#        print('after')
-#        print(tempTr)
     
     def getWaitingTimes(self):
         return self.waitingTimeArray
@@ -301,7 +294,6 @@ class PostModel():
     def __init__(self, initTime, tillCount, separateValue):
         self.initTime = initTime
         self.inputQueueEngine = InputQueueEngine(initTime)
-#        self.inputQueueEngine.printStat()
         self.tillCount = tillCount
         self.separateValue = separateValue
         self.tillEngine = TillEngine(tillCount)
@@ -310,7 +302,6 @@ class PostModel():
     def start(self):
         tempTime = 0
         while(tempTime < self.initTime):
-#            print("time now = " + str(tempTime))
 #           попытка добавить операцию в очередь, проверка очереди прихода
             tempOper = self.inputQueueEngine.tryToPopFromQueue()
 #            self.genQueue.getStat()
@@ -328,14 +319,13 @@ class PostModel():
 #            добавляет время ожидания всем в очереди
             self.genQueue.increaseAllTimes()
             tempTime += 1
-            
-#        self.inputQueueEngine.printStat()
         
     def getTillsStat(self):
         self.tillEngine.getStat()
         
     def getStatDict(self):
         waitingTimes = self.genQueue.getWaitingTimes()
+#        print(waitingTimes)
         return {'tillCount'      : self.tillCount,
                 'separateValue'  : self.separateValue,
                 'maxWaitingTime' : max(waitingTimes),
@@ -351,16 +341,15 @@ class PostModel():
         print("Максимальная длина очереди = " + str(stat['maxQueueLen']))
         print("Не успели обслужиться - " + str(stat['countIgnored']))
         print("Среднее время ожидания = " + str(stat['serviceMidTime']))
-#        print(self.genQueue.getWaitingTimes())
 
 #создание модели для почты, параметры - время, количество точек обслуживания   
 generalTimeToModel = 80
 
 class OptimalParameters():
-    def __init__(self):     
-        self.modelTime = 80
-        self.tillCountMax = 5
-        self.separateValueMax = 10
+    def __init__(self, modelTime, tillCountMax, separateValueMax):    
+        self.modelTime = modelTime
+        self.tillCountMax = tillCountMax
+        self.separateValueMax = separateValueMax
         self.listOfStat = []
         
     def tryToOptimise(self): 
@@ -368,22 +357,60 @@ class OptimalParameters():
         while(tillCount <= self.tillCountMax):
             separateValue = 1
             while(separateValue <= self.separateValueMax):
-                posMod = PostModel(self.modelTime, tillCount, separateValue)
-                posMod.start() 
-                self.listOfStat.append(posMod.getStatDict())
+                objTemp = MultipleTesting(self.modelTime, tillCount, separateValue)
+                self.listOfStat.append(objTemp.getStat())
                 separateValue += 1
-            tillCount += 1
-        print(self.listOfStat)               
+            tillCount += 1              
 
-    def getOptimalParameters(self):
-        print()
+    def getOptimalParameters(self, optimalType):
+        self.tryToOptimise()
+        theMostOptimal = None
+        if(len(self.listOfStat) > 0):
+            theMostOptimal = self.listOfStat[0]
+            for statObj in self.listOfStat:
+                if(statObj[optimalType] < theMostOptimal[optimalType]):
+                    theMostOptimal = statObj
+        return theMostOptimal
         
-#testProgon = OptimalParameters()
-#testProgon.tryToOptimise()
-                
-#        print(posMod.getStatDict())      
-#        posMod.getUserStat()  
-#        posMod.getTillsStat()
+
+class MultipleTesting():
+    def __init__(self, modelTime, tillCount, separateValue):
+        self.modelTime = modelTime
+        self.tillCount = tillCount
+        self.separateValue = separateValue
+        self.resultArray = self.calculate(self.modelTime, self.tillCount, self.separateValue)
+    
+    def calculate(self, modelTime, tillCount, separateValue):
+        resultArray = []
+        tempI = 0
+        while(tempI < repeatCount):
+            tempPosMod = PostModel(modelTime, tillCount, separateValue)
+            tempPosMod.start()
+            resultArray.append(tempPosMod.getStatDict())
+            tempI += 1
+        return resultArray
+            
+    def getStat(self):
+        maxWaitingTime = 0
+        minWaitingTime = 0
+        maxQueueLen = 0
+        countIgnored = 0
+        serviceMidTime = 0
+        for singleStat in self.resultArray:
+            maxWaitingTime += singleStat['maxWaitingTime']
+            minWaitingTime += singleStat['minWaitingTime']
+            maxQueueLen += singleStat['maxQueueLen']
+            countIgnored += singleStat['countIgnored']
+            serviceMidTime += singleStat['serviceMidTime']
+        statsLen = len(self.resultArray)
+        return {'tillCount'      : self.tillCount,
+                'separateValue'  : self.separateValue,
+                'maxWaitingTime' : maxWaitingTime / statsLen,
+                'minWaitingTime' : minWaitingTime / statsLen,
+                'maxQueueLen'    : maxQueueLen / statsLen,
+                'countIgnored'   : countIgnored / statsLen,
+                'serviceMidTime' : serviceMidTime / statsLen}
+
 
 class GeneralFrame(Frame):
     def __init__(self, parent):
@@ -412,31 +439,57 @@ class GeneralFrame(Frame):
         timeField = Entry(frame, font='Arial 10', textvariable=self.modelTime)
         timeField.grid(row = 2, column = 1, columnspan = 1,  padx=5, pady=5)
 
-        button_accept = Button(frame, text = 'Рассчитать', command = self.calculateFunc)
-        button_accept.config(background="#96cafb")
-        button_accept.grid(row = 3, column = 1, columnspan = 1,  padx=5, pady=5)
+        button_calc = Button(frame, text = 'Рассчитать', command = self.calculateFunc)
+        button_calc.config(background="#96cafb")
+        button_calc.grid(row = 3, column = 0, columnspan = 1,  padx=5, pady=5)
+        
+        button_optim = Button(frame, text = 'Оптимизировать', command = self.optimise)
+        button_optim.config(background="#96cafb")
+        button_optim.grid(row = 3, column = 1, columnspan = 1,  padx=5, pady=5)
         
         
-#        
-#        button_about = Button(frame, text = 'Что-то', command = self.calculateFunc)
-#        button_about.config(width="36",height="36", background="#96cafb")
-#        button_about.pack(side = RIGHT, padx=5, pady=5) 
         frame.mainloop()
+        
+    def optimise(self):
+        tillCount = int(self.tillCount.get())
+        separateValue = int(self.separateValue.get())
+        modelTime = int(self.modelTime.get())
+        optimPar = OptimalParameters(modelTime, tillCount, separateValue)
+        self.formReportFrame(optimPar.getOptimalParameters('maxWaitingTime'), 1)
         
     def calculateFunc(self):
         tillCount = int(self.tillCount.get())
         separateValue = int(self.separateValue.get())
-        modelTime = int(self.modelTime.get())
-        postModel = PostModel(modelTime, tillCount, separateValue)
-        postModel.start()
-        print(postModel.getStatDict())
-#        testProgon = OptimalParameters()
-#        testProgon.tryToOptimise()
-        print(self.tillCount.get())
-        print(self.separateValue.get())
-        print(self.modelTime.get())
+        modelTime = int(self.modelTime.get())       
+        objTemp = MultipleTesting(modelTime, tillCount, separateValue)
+        self.formReportFrame(objTemp.getStat())
         
+    def formReportFrame(self, result, optimMode = None):
+        resFrame = Toplevel()
+        resFrame.title("Результат")
+        if(optimMode):
+            labelTableName = Label(resFrame, text="В резульате оптимазации найдены наименьшие значения:")
+        else:
+            labelTableName = Label(resFrame, text="Для указанных параметров получены результаты:")
+        widthFrame = 170
+        if(labelTableName.winfo_reqwidth() + 10 > widthFrame):
+            widthFrame = labelTableName.winfo_reqwidth() + 10
+        resFrame.geometry(str(widthFrame) + "x200")
+        labelTableName.grid(row = 0, column = 0, columnspan = 3, padx=5, pady=5)
+        textToLable = "Количество точек обслужвания = " + str(result['tillCount'])
+        textToLable += "\nСегментация очереди = " + str(result['separateValue'])
+        textToLable += "\nМаксимальное время ожидания = " + str(result['maxWaitingTime'])
+        textToLable += "\nМинимальное время ожмдания = " + str(result['minWaitingTime'])
+        textToLable += "\nМаксимальная длина очереди = " + str(result['maxQueueLen'])
+        textToLable += "\nНе успели обслужиться - " + str(result['countIgnored'])
+        textToLable += "\nСреднее время ожидания = " + str(result['serviceMidTime'])
+        labelStat = Label(resFrame, text = textToLable).grid(row = 1, column = 0, rowspan = 6, columnspan = 3, padx=5, pady=5)
 
+        def okay():
+            resFrame.destroy()
+        button_decine = Button(resFrame, text = '   Ок   ', command = okay)
+        button_decine.grid(row = 10, column = 1, padx=5, pady=5) 
+        
 root=Tk()
 var = StringVar()
 root.geometry("350x300")
